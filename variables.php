8362cfee2bd327da81ce3fc9bd623907b9652d5e
Hosni19 <hosnihamdi9999@gmail.com>
2023-01-03 11:53:55 +0100

    nouveau type de variables : les entier
    Les entiers

--- variables.php
+++ variables.php
@@ -51,3 +51,90 @@ $autrephrase.=$piece ;
 
 echo $sauteDeLigne;
 echo $autrephrase;
+
+
+//-------------------------------------------------
+
+//deuxieme type de variables : les nombres ENTIERS
+
+$unNombre = 35 ;
+$unAutreNombre = 11 ;
+
+//php n'est tres bon pour gerer les grands nombres 
+
+
+
+
+$nombreNegatif = -7 ;
+echo $sauteDeLigne; 
+
+$resultat = $unNombre + $unAutreNombre ;
+echo $resultat ; 
+echo $sauteDeLigne; 
+
+$resultat = $unNombre + 7 ; 
+echo $resultat ; 
+echo $sauteDeLigne; 
+
+$resultat = $unNombre % 2 ; 
+echo $resultat ; 
+echo $sauteDeLigne;
+
+$resultat = $unNombre + $unAutreNombre +$unNombre + $unAutreNombre ; 
+echo $resultat ; 
+echo $sauteDeLigne;
+
+
+// on peut faire des operations mathematiques aussi complexes que necessaires .
+
+$patricipants = 20 ;
+$tailleDeGroupe = 3 ;
+
+$participantsSansGroupe = $patricipants % $tailleDeGroupe ;
+
+
+
+
+echo 'Le nombre des participants sans groupe est : ' .$participantsSansGroupe ;
+echo $sauteDeLigne;
+
+$nombreEtudiant = 25 ;
+$nombreEtudiantOptionDev=6 ;
+
+$pourcentageEtudianntOptionDev = (6/25)*100 .'%' ;
+
+echo $pourcentageEtudianntOptionDev ;
+
+
+$somme = 0;
+$somme = $somme + 2 ;
+$somme = $somme +5 ;
+$somme = $somme + 6 ;
+$somme = $somme + 7 ;
+
+
+//version condensé 
+
+$somme = 0 ;
+$somme += 2 ;
+$somme += 5 ;
+$somme += 6 ;
+$somme += 7 ;
+
+// += et -= sont souvent utilisés 
+//les autres beaucoup moins
+
+//operateurs d'incrementation et decrementation 
+
+$compteur = 5 ;
+$compteur++ ;
+
+$autreCompteur = 22 ;
+$autreCompteur-- ;
+
+
+// ces 3 lignes font la meme chose 
+
+$a = $a + 1 ; 
+$a+=1 ;
+$a++ ;
